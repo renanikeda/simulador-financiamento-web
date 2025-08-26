d870bcda1e9148cdab1fcf7685bbbcf577ad3806
renan.ikeda <renan.ikeda@ibotz.com.br>
2025-08-26 18:00:09 -0300

    Adding interactivity

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useReducer, useState } from 'react'
 import './App.css'
 import './components/AmortizationTable'
 import AmortizationTable from './components/AmortizationTable'
+import ClipLoader from "react-spinners/ClipLoader";
 import Financiamento from './engine/Engine'
 import optionsReducer, { initialOptions, OptionsContext, OptionsDispatchContext } from './Reducer'
 import type { optionsHeader, Parcela } from './utils'
@@ -11,10 +12,17 @@ import OptionsHeader from './components/OptionsHeader'
 function App() {
   const [options, dispatch] = useReducer(optionsReducer, initialOptions);
   const [installments, setInstallments] = useState<Parcela[]>([]);
+  const [loading, setLoading] = useState(false);
   
   const calculateInstallments = (options: optionsHeader) => {
-    const loan = new Financiamento(options.propertyValue, options.downPayment, options.interestRate, options.TRRate, options.termYears, options.extraAmortization, options.totalPayment);
-    setInstallments(loan.calcularSAC());
+    setLoading(true);
+    setInstallments([]);
+
+    setTimeout(() => {
+      const loan = new Financiamento(options.propertyValue, options.downPayment, options.interestRate, options.TRRate, options.termYears, options.extraAmortization, options.totalPayment);
+      setLoading(false);
+      setInstallments(loan.calcularSAC());
+    }, 250)
   }
   
   return (
@@ -23,6 +31,9 @@ function App() {
         <OptionsDispatchContext value={dispatch}>
           <OptionsHeader calculateInstallments={calculateInstallments}/>
           <AmortizationTable installments={installments} />
+          {loading && (
+            <ClipLoader size={50} color="primary" />
+          )}
         </OptionsDispatchContext>
       </OptionsContext>
     </>
